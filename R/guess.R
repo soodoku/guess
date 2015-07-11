dca7d05298daca02b37a68a84e9584e4bf0fbbb1
Gaurav Sood <gsood07@gmail.com>
2015-07-11 19:26:45 -0400

    basic package file

---
+++ R/guess.R
@@ -0,0 +1,6 @@
+#' \pkg{guess} adjusts estimates of learning for guessing related bias.
+#'
+#' It implements the method discussed in \url{http://gsood.com/research/papers/guess.pdf}
+#' @name guess
+#' @docType package
+NULL
