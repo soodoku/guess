307fe083dbe81f8344480a11d601a64562016a48
Gaurav Sood <gsood07@gmail.com>
2015-07-17 13:25:49 -0400

    Revised checks and amended df return

--- R/stndcor.R
+++ R/stndcor.R
@@ -1,15 +1,16 @@
 #' Standard Guessing Correction for Learning
 #'
 #' Estimate of learning adjusted with standard correction for guessing. Correction is based on number of options per question.
-#' Requirements: separate pre-test and post-test dataframes. Why do we need dataframes? To accomodate multiple items.
-#' The items can carry NA (missing). Items must be in the same order in each dataframe. For now we assume that respondents are posed same questions twice. 
-#' The function also takes a lucky vector - chance of getting a correct answer if guessing randomly. Each entry is 1/(no. of options).
-#' By default, the vector of corrected scores that is returned has same item names as the pre_test items.  
+#' The function takes separate pre-test and post-test dataframes. Why do we need dataframes? To accomodate multiple items.
+#' The items can carry NA (missing). Items must be in the same order in each dataframe. Assumes that respondents are posed same questions twice. 
+#' The function also takes a lucky vector -- the chance of getting a correct answer if guessing randomly. Each entry is 1/(no. of options).
+#' The function also optionally takes a vector carrying names of the items. By default, the vector carrying adjusted learning estimates takes same item names as the pre_test items.  
 #' However you can assign a vector of names separately via item_names.
-#' @param pre_test data.frame carrying responses to pre-test questions.
-#' @param pst_test data.frame carrying responses to post-test questions.
-#' @param lucky 
-#' @return a numeric vector containing adjusted estimates of learning for each item
+#' @param pre_test Required. data.frame carrying responses to pre-test questions.
+#' @param pst_test Required. data.frame carrying responses to post-test questions.
+#' @param lucky    Required. A vector. Each entry is 1/(no. of options)
+#' @param item_names Optional. A vector carrying item names.
+#' @return   a list of three vectors, carrying pre-treatment corrected scores, post-treatment scores, and adjusted estimates of learning
 #' @export
 #' @examples
 #' pre_test <- data.frame(item1=c(1,0,0,1,0), item2=c(1,NA,0,1,0)); pst_test <-  pre_test + cbind(c(0,1,1,0,0), c(0,1,0,0,1))
@@ -17,20 +18,20 @@
  
 stndcor <- function(pre_test=NULL, pst_test=NULL, lucky=NULL, item_names=NULL)
 {	
-	if (is.null(pre_test)) stop("Specify pre_test data.frame.") # pre_test data frame is missing
-	if (is.null(pst_test)) stop("Specify pst_test data.frame.") # post_test data frame is missing
-	if (is.null(lucky)) stop("Specify lucky vector.")           # lucky vector is missing
+	if (!is.data.frame(pre_test)) stop("Specify pre_test data.frame.") # pre_test data frame is missing
+	if (!is.data.frame(pst_test)) stop("Specify pst_test data.frame.") # post_test data frame is missing
+	if (is.null(lucky))    stop("Specify lucky vector.")           # lucky vector is missing
 	
-	if(length(unique(length(pre_test), length(pst_test), length(lucky)))!=1) stop("Length of input varies. Length of pre_test, pst_test, and lucky must be the same.")
+	if (length(unique(length(pre_test), length(pst_test), length(lucky)))!=1) stop("Length of input varies. Length of pre_test, pst_test, and lucky must be the same.")
 
-	n_items <- length(pre_test)
-	pre_test_cor  <- pst_test_cor <- stnd_cor <- rep(NA, length=n_items)
+	n_items <- length(pre_test) # total number of items
+	pre_test_cor  <- pst_test_cor <- stnd_cor <- rep(NA, length=n_items) # initialize some results vectors
 	
 	# Names of the return vector
 	if (is.null(item_names)) {
-		names(stnd_cor) <- names(pre_test)
+		names(pre_test_cor) <- names(pst_test_cor) <- names(stnd_cor) <- names(pre_test)
 	} else {
-		names(stnd_cor) <- item_names
+		names(pre_test_cor) <- names(pst_test_cor) <- names(stnd_cor) <- item_names
 	}
 
 	for (i in 1:n_items)
@@ -40,5 +41,9 @@ stndcor <- function(pre_test=NULL, pst_test=NULL, lucky=NULL, item_names=NULL)
 		stnd_cor[i]     <- pst_test_cor[i] - pre_test_cor[i]
 	}
 
-	stnd_cor/nrow(pre_test)
+	pre   <- pre_test_cor/nrow(pre_test)
+	pst   <- pst_test_cor/nrow(pst_test)
+	learn <- stnd_cor/nrow(pre_test)
+
+	return(list(pre=pre, pst=pst, learn=learn))
 }
