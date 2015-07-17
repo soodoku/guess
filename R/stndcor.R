618d5025d4f0ca3907afd62f0356f78ad168d713
Gaurav Sood <gsood07@gmail.com>
2015-07-17 13:09:11 -0400

    return named vector

--- R/stndcor.R
+++ R/stndcor.R
@@ -4,6 +4,8 @@
 #' Requirements: separate pre-test and post-test dataframes. Why do we need dataframes? To accomodate multiple items.
 #' The items can carry NA (missing). Items must be in the same order in each dataframe. For now we assume that respondents are posed same questions twice. 
 #' The function also takes a lucky vector - chance of getting a correct answer if guessing randomly. Each entry is 1/(no. of options).
+#' By default, the vector of corrected scores that is returned has same item names as the pre_test items.  
+#' However you can assign a vector of names separately via item_names.
 #' @param pre_test data.frame carrying responses to pre-test questions.
 #' @param pst_test data.frame carrying responses to post-test questions.
 #' @param lucky 
@@ -13,17 +15,24 @@
 #' pre_test <- data.frame(item1=c(1,0,0,1,0), item2=c(1,NA,0,1,0)); pst_test <-  pre_test + cbind(c(0,1,1,0,0), c(0,1,0,0,1))
 #' lucky <- rep(.25, 2); stndcor(pre_test, pst_test, lucky)
  
-stndcor <- function(pre_test=NULL, pst_test=NULL, lucky=NULL)
+stndcor <- function(pre_test=NULL, pst_test=NULL, lucky=NULL, item_names=NULL)
 {	
-	if(is.null(pre_test)) stop("Specify pre_test data.frame.")
-	if(is.null(pst_test)) stop("Specify pst_test data.frame.")
-	if(is.null(lucky)) stop("Specify lucky vector.")
+	if (is.null(pre_test)) stop("Specify pre_test data.frame.") # pre_test data frame is missing
+	if (is.null(pst_test)) stop("Specify pst_test data.frame.") # post_test data frame is missing
+	if (is.null(lucky)) stop("Specify lucky vector.")           # lucky vector is missing
 	
 	if(length(unique(length(pre_test), length(pst_test), length(lucky)))!=1) stop("Length of input varies. Length of pre_test, pst_test, and lucky must be the same.")
 
 	n_items <- length(pre_test)
-	pre_test_cor  <- pst_test_cor <- stnd_cor <- matrix(ncol=1, nrow=n_items)
-		
+	pre_test_cor  <- pst_test_cor <- stnd_cor <- rep(NA, length=n_items)
+	
+	# Names of the return vector
+	if (is.null(item_names)) {
+		names(stnd_cor) <- names(pre_test)
+	} else {
+		names(stnd_cor) <- item_names
+	}
+
 	for (i in 1:n_items)
 	{
 		pre_test_cor[i] <- sum(pre_test[,i], na.rm = TRUE) - length(which(pre_test[,i] == 0))/(1/lucky[i] - 1)
