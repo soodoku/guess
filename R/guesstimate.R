409b6ed347af873c5d6d27cce7a51a029a821d2a
Gaurav Sood <gsood07@gmail.com>
2016-02-10 18:33:58 -0500

    allow people to pass priors

--- R/guesstimate.R
+++ R/guesstimate.R
@@ -2,6 +2,8 @@
 
 #' @title Calculate item level and aggregate learning
 #' @param transmatrix  transition matrix returned from \code{\link{multi_transmat}}
+#' @param nodk_priors Optional. Vector of length 4. Priors for the parameters for model that fits data without Don't Knows
+#' @param dk_priors  Optional. Vector of length 8. Priors for the parameters for model that fits data with Don't Knows
 #' @return list with two items: parameter estimates and estimates of learning
 #' @export
 #' @examples
@@ -11,27 +13,32 @@
 #' transmatrix <- transmat(pre_test_var, pst_test_var)
 #' res <- guesstimate(transmatrix)
 
-guesstimate <- function(transmatrix=NULL) {
+guesstimate <- function(transmatrix=NULL, nodk_priors=c(.3,.1,.1,.25), dk_priors=c(.3,.1,.2,.05,.1,.1,.05,.25)) 
+{
 		
 	# Initialize results mat
 	nitems	<- nrow(transmatrix)
 	nparams <- ifelse(ncol(transmatrix)==4, 4, 8)
 	est.opt <- matrix(ncol=nitems, nrow=nparams)
 	
+	# priors
+	nodk_priors <- prior4
+	dk_priors   <- prior8
+
 	# effects
 	effects	<- matrix(ncol=nitems, nrow=1)
 
 	# calculating parameter estimates
 	if (nparams == 4) {
 		for (i in 1:nitems) {
-			est.opt[,i]	 <- tryCatch(solnp(c(.3,.1,.1,.25), guess_lik, eqfun = eqn1, eqB = c(1), LB = rep(0,4), UB = rep(1,4), data=transmatrix[i,])[[1]], error=function(e) NULL)
+			est.opt[,i]	 <- tryCatch(solnp(nodk_priors, guess_lik, eqfun = eqn1, eqB = c(1), LB = rep(0,4), UB = rep(1,4), data=transmatrix[i,])[[1]], error=function(e) NULL)
 		}
 
 		effects[,1:nitems] <- est.opt[2,]
 
 	} else {
 		for (i in 1:nitems) {
-			est.opt[,i]	 <- tryCatch(solnp(c(.3,.1,.2,.05,.1,.1,.05,.25), guessdk_lik, eqfun = eqn1dk, eqB = c(1), LB = rep(0,8), UB = rep(1,8), data=transmatrix[i,])[[1]], error=function(e) rep(NA,8))
+			est.opt[,i]	 <- tryCatch(solnp(dk_priors, guessdk_lik, eqfun = eqn1dk, eqB = c(1), LB = rep(0,8), UB = rep(1,8), data=transmatrix[i,])[[1]], error=function(e) rep(NA,8))
 		}
 		
 		effects[,1:nitems] 	<- est.opt[2,] + est.opt[6,]
